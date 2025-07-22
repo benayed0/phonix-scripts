41f270abeaa2d9a5ab03eecda3974a9514a426ea
Ubuntu <ubuntu@ip-172-31-13-237.eu-west-3.compute.internal>
2025-07-22 11:02:16 +0000

    adding i18n serving

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { join } from 'path';
+import * as express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: '*' },
   });
   await app.listen(3000, '0.0.0.0');
+  app.use('/assets', express.static(join(__dirname, '..', 'assets')));
+
   process.on('SIGTERM', async () => {
     console.log('SIGTERM signal received: closing HTTP server');
     await app.close();
